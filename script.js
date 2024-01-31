0240ec01cf2d12b1b673123e1875678f5a5f5ce4
arianchhim978 <147645634+arianchhim978@users.noreply.github.com>
2024-01-31 00:07:06 -0500

    created the basic website for my portfolio

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        var header = document.querySelector("header");
+        
+        if (window.scrollY > 0) {
+            header.style.backgroundColor = "#555"; // Change background color on scroll
+        } else {
+            header.style.backgroundColor = "#333"; // Change back to original color when at the top
+        }
+    });
+});
